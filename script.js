b7fec6242a89e5521e2aef8e96ffcc2f34e970f1
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-12 05:26:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr) {
+    if (arr.length <= 1) return 0;
 
-module.exports=mincost;
+    // Convert to min-heap using sort (simple for this problem)
+    arr.sort((a, b) => a - b);
+
+    let totalCost = 0;
+
+    while (arr.length > 1) {
+        // take two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let cost = first + second;
+        totalCost += cost;
+
+        // insert back in sorted position
+        // push then sort (simple for n <= 1000)
+        arr.push(cost);
+        arr.sort((a, b) => a - b);
+    }
+
+    return totalCost;
+}
