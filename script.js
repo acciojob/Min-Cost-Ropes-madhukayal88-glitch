8c516d0e1421e7e838747372d916d94df1e990d0
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-12 05:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,71 @@
-function mincost(arr) {
-    if (arr.length <= 1) return 0;
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-    // Convert to min-heap using sort (simple for this problem)
-    arr.sort((a, b) => a - b);
+  // Helper functions
+  parent(i) { return Math.floor((i - 1) / 2); }
+  left(i) { return 2 * i + 1; }
+  right(i) { return 2 * i + 2; }
 
-    let totalCost = 0;
+  // Insert element
+  push(val) {
+    this.heap.push(val);
+    this.bubbleUp(this.heap.length - 1);
+  }
 
-    while (arr.length > 1) {
-        // take two smallest ropes
-        let first = arr.shift();
-        let second = arr.shift();
-
-        let cost = first + second;
-        totalCost += cost;
-
-        // insert back in sorted position
-        // push then sort (simple for n <= 1000)
-        arr.push(cost);
-        arr.sort((a, b) => a - b);
+  bubbleUp(i) {
+    while (i > 0 && this.heap[i] < this.heap[this.parent(i)]) {
+      [this.heap[i], this.heap[this.parent(i)]] = [this.heap[this.parent(i)], this.heap[i]];
+      i = this.parent(i);
     }
+  }
 
-    return totalCost;
+  // Extract min
+  pop() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown(0);
+    return root;
+  }
+
+  bubbleDown(i) {
+    let smallest = i;
+    const left = this.left(i);
+    const right = this.right(i);
+
+    if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+      smallest = left;
+    }
+    if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+      smallest = right;
+    }
+    if (smallest !== i) {
+      [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
+      this.bubbleDown(smallest);
+    }
+  }
+
+  size() { return this.heap.length; }
+}
+
+function mincost(arr) {
+  if (arr.length <= 1) return 0;
+
+  const heap = new MinHeap();
+  for (let rope of arr) heap.push(rope);
+
+  let totalCost = 0;
+  while (heap.size() > 1) {
+    let first = heap.pop();
+    let second = heap.pop();
+    let cost = first + second;
+    totalCost += cost;
+    heap.push(cost);
+  }
+
+  return totalCost;
 }
