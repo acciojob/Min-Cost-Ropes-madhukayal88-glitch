960001ce59848b3685a80f4724199b53c8fa4c17
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-16 05:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 function mincost(arr) {
     if (arr.length <= 1) return 0;
 
-    // Min Heap using sort (acceptable for constraints here)
-    arr.sort((a, b) => a - b);
-
+    // Min heap using sort
+    let heap = [...arr].sort((a, b) => a - b);
     let cost = 0;
 
-    while (arr.length > 1) {
-        let first = arr.shift();
-        let second = arr.shift();
+    while (heap.length > 1) {
+        let first = heap.shift();
+        let second = heap.shift();
 
         let sum = first + second;
         cost += sum;
 
-        // Insert back and keep sorted
-        arr.push(sum);
-        arr.sort((a, b) => a - b);
+        heap.push(sum);
+        heap.sort((a, b) => a - b);
     }
 
     return cost;
