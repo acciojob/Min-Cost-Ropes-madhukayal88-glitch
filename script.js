9f24ae8b676bdfe75663977c8ebc3201413876c3
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-19 05:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 function mincost(arr) {
-    // If only one or no rope, cost is 0
     if (arr.length <= 1) return 0;
 
-    // Work on a copy to avoid modifying input
     let ropes = [...arr];
     let totalCost = 0;
 
-    // Initial sort
     ropes.sort((a, b) => a - b);
 
     while (ropes.length > 1) {
-        // Pick two smallest ropes
-        let first = ropes.shift();
-        let second = ropes.shift();
+        let a = ropes.shift();
+        let b = ropes.shift();
 
-        let sum = first + second;
+        let sum = a + b;
         totalCost += sum;
 
-        // Insert sum back and keep array sorted
         let i = 0;
         while (i < ropes.length && ropes[i] < sum) {
             i++;
