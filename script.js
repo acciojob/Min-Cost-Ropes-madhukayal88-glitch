354ab4d6d2cabcd3a0956982af7e504f38dc2cc9
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-19 05:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function mincost(arr) {
     if (arr.length <= 1) return 0;
 
-    let ropes = [...arr];
+    let ropes = [...arr];   // input mutate aagakoodadhu
     let cost = 0;
 
-    ropes.sort((a, b) => a - b);
+    ropes.sort((a, b) => a - b);  // sort knowledge use pannrom
 
     while (ropes.length > 1) {
-        let a = ropes.shift();
-        let b = ropes.shift();
+        let x = ropes.shift();
+        let y = ropes.shift();
 
-        let sum = a + b;
+        let sum = x + y;
         cost += sum;
 
         let i = 0;
