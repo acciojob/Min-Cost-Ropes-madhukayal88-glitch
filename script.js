2b4d7a7d567b2d625a228f69197b1f6bbf46fc00
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-19 05:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 function mincost(arr) {
-    if (arr.length <= 1) {
-        return 0;
-    }
-
-    var ropes = arr.slice(); // copy array
     var cost = 0;
 
-    ropes.sort(function(a, b) {
-        return a - b;
-    });
+    while (arr.length > 1) {
+        arr.sort(function(a, b) {
+            return a - b;
+        });
 
-    while (ropes.length > 1) {
-        var x = ropes.shift();
-        var y = ropes.shift();
+        var first = arr.shift();
+        var second = arr.shift();
 
-        var sum = x + y;
+        var sum = first + second;
         cost += sum;
 
-        var i = 0;
-        while (i < ropes.length && ropes[i] < sum) {
-            i++;
-        }
-        ropes.splice(i, 0, sum);
+        arr.push(sum);
     }
 
     return cost;
