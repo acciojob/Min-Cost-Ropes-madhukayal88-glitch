7d79c99d28ad29c82bf8e5b78569670fa6551968
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-27 05:36:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
-// Function name must match exactly what index.js is calling
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  push(val) {
+    this.heap.push(val);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.size() === 1) return this.heap.pop();
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return min;
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[parentIndex] <= this.heap[index]) break;
+      [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+      index = parentIndex;
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+    while (true) {
+      let left = 2 * index + 1;
+      let right = 2 * index + 2;
+      let smallest = index;
+
+      if (left < this.size() && this.heap[left] < this.heap[smallest]) smallest = left;
+      if (right < this.size() && this.heap[right] < this.heap[smallest]) smallest = right;
+      if (smallest === index) break;
+
+      [this.heap[smallest], this.heap[index]] = [this.heap[index], this.heap[smallest]];
+      index = smallest;
+    }
+  }
+}
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
 function minCost(arr) {
   if (!arr || arr.length <= 1) return 0;
 
-  // Use a greedy approach: always combine the two smallest ropes
-  // For N=1000, we can sort in each step or use a priority queue logic
+  const heap = new MinHeap();
+  // Add all rope lengths to the heap
+  arr.forEach(len => heap.push(len));
+
   let totalCost = 0;
 
-  // Sort initially
-  arr.sort((a, b) => a - b);
+  // While there is more than one rope to connect
+  while (heap.size() > 1) {
+    // Extract the two shortest ropes
+    const first = heap.pop();
+    const second = heap.pop();
 
-  while (arr.length > 1) {
-    // Take the two smallest
-    let first = arr.shift();
-    let second = arr.shift();
+    const cost = first + second;
+    totalCost += cost;
 
-    let currentSum = first + second;
-    totalCost += currentSum;
-
-    // Insert the sum back into the array while maintaining sort order
-    // This is more efficient than calling .sort() every time
-    let inserted = false;
-    for (let i = 0; i < arr.length; i++) {
-      if (currentSum < arr[i]) {
-        arr.splice(i, 0, currentSum);
-        inserted = true;
-        break;
-      }
-    }
-    if (!inserted) arr.push(currentSum);
+    // Put the combined rope back into the heap
+    heap.push(cost);
   }
 
   return totalCost;
 }
 
-// Ensure you export it if it's in a separate file
+// Export for the test runner
 module.exports = minCost;
